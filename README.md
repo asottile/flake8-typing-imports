cb5bdbf0a6d3844d401c8b638797421a12207643
Anthony Sottile <asottile@umich.edu>
2019-05-20 19:25:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+[![Build Status](https://dev.azure.com/asottile/asottile/_apis/build/status/asottile.flake8-typing-imports?branchName=master)](https://dev.azure.com/asottile/asottile/_build/latest?definitionId=23&branchName=master)
+[![Azure DevOps coverage](https://img.shields.io/azure-devops/coverage/asottile/asottile/23/master.svg)](https://dev.azure.com/asottile/asottile/_build/latest?definitionId=23&branchName=master)
+
+flake8-typing-imports
+=====================
+
+flake8 plugin which checks that typing imports are properly guarded
+
+## installation
+
+`pip install flake8-typing-imports`
+
+## rationale
+
+depending on your supported version of python, you may need to guard your
+imports by `if TYPE_CHECKING:` (3.5.2+) or `if False:` if the things you are
+importing aren't available in all the pythons you support.
+
+unfortunately, the `typing` module has been pretty unstable -- it has seen api
+changes in 3.5.0, 3.5.2, 3.5.3, 3.5.4, 3.6.0, 3.6.1, 3.6.2, and 3.7.0!
+
+as it's pretty difficult to keep track of what version things changed and you
+can't always test against particular patch versions of python, this plugin
+helps you statically check this automatically!
+
+## configuration
+
+this plugin has a single configuration point (beyond those provided by flake8)
+which is the `--min-python-version` option.
+
+by default, this option is `3.5.0`.  this includes all versions of python
+which have the `typing` module present.
+
+you can also set this option in the flake8 configuration if you don't want
+to use the commandline:
+
+```ini
+[flake8]
+min_python_version = 3.6.2
+```
+
+## as a pre-commit hook
+
+See [pre-commit](https://github.com/pre-commit/pre-commit) for instructions
+
+Sample `.pre-commit-config.yaml`:
+
+```yaml
+-   repo: https://gitlab.com/pycqa/flake8
+    rev: 3.7.7
+    hooks:
+    -   id: flake8
+        additional_dependencies: [flake8-typing-imports==0.0.0]
+```
---
+++ tests/flake8_typing_imports_test.py
@@ -0,0 +1,87 @@
+import ast
+from unittest import mock
+
+import pytest
+from flake8.options.manager import OptionManager
+
+from flake8_typing_imports import Plugin
+from flake8_typing_imports import Version
+
+
+def version_ctx(v):
+    return mock.patch.object(Plugin, '_min_python_version', v)
+
+
+@pytest.fixture(autouse=True)
+def reset_version():
+    with version_ctx(Plugin._min_python_version):
+        yield
+
+
+def test_option_parsing():
+    mgr = OptionManager('flake8', '0')
+    Plugin.add_options(mgr)
+    options, _ = mgr.parse_args(['--min-python-version', '3.6.2'])
+    Plugin.parse_options(options)
+    assert Plugin._min_python_version == Version(3, 6, 2)
+
+
+def test_option_parsing_error_too_old():
+    with pytest.raises(ValueError) as excinfo:
+        Plugin.parse_options(mock.Mock(min_python_version='3.4'))
+    msg, = excinfo.value.args
+    assert msg == 'min-python-version (3.4.0): must be >= 3.5'
+
+
+def test_option_parsing_error_unknown():
+    with pytest.raises(ValueError) as excinfo:
+        Plugin.parse_options(mock.Mock(min_python_version='9.9'))
+    msg, = excinfo.value.args
+    assert msg == 'min-python-version (9.9.0): unknown version'
+
+
+def results(s):
+    return {'{}:{}: {}'.format(*r) for r in Plugin(ast.parse(s)).run()}
+
+
+def test_missing_guard():
+    with version_ctx(Version(3, 5, 0)):
+        assert results('from typing import Type') == {
+            '1:0: TYP001 guard import by `if False:  # TYPE_CHECKING`: Type '
+            '(not in 3.5.0, 3.5.1)',
+        }
+
+
+def test_missing_guard_3_5_1():
+    with version_ctx(Version(3, 5, 1)):
+        assert results('from typing import Type') == {
+            '1:0: TYP001 guard import by `if False:  # TYPE_CHECKING`: Type '
+            '(not in 3.5.1)',
+        }
+
+
+def test_guard_by_False():
+    ret = results(
+        'if False:\n'
+        '    from typing import Type',
+    )
+    assert ret == set()
+
+
+def test_guard_by_assignment():
+    ret = results(
+        'MYPY = False\n'
+        'if MYPY:\n'
+        '    from typing import Type',
+    )
+    assert ret == set()
+
+
+def test_guard_by_type_checking():
+    with version_ctx(Version(3, 7, 0)):
+        ret = results(
+            'from typing import TYPE_CHECKING\n'
+            'if TYPE_CHECKING:\n'
+            '    from typing import DEFINITELY_WRONG\n',
+        )
+        assert ret == set()
